e3ed22476c3cde9cdd2ee7e0a5e5f5df8be1e02e
dibyenduweb <chottu668@gmail.com>
2024-06-03 01:40:40 +0530

    update

---
+++ src/Title.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+function Title() {
+    let name = "dibyendu"
+    return(
+        <div>
+             <h1>Title</h1>
+             <p>hi,{name}</p>
+        </div>
+    )}
+
+export default Title;
+
