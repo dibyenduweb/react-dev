2d401fcdcc586f4f1dd2592e75008d62e5f9005c
Dibyendu Pramanik <chotuu668@gmil.com>
2024-06-05 21:00:23 +0530

    update

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,18 @@
-import "./Product.css"
-function Product({title,price}) {
-    return(
-        <div className="Product">
-            <h3>{title}</h3>
-            <h3>{price}</h3>
-            <h5>Product Descripton</h5>
-        </div>
-    );
+/* eslint-disable react/prop-types */
+import "./Product.css";
+
+function Product({ title, price,  }) {
+  // const list = features.map((feature)=><li>{feature}</li>)
+let styles = {backgroundColor :price >30000 ? "pink" : ""};
+  return (
+    <div className="Product" style={styles}>
+      <h3>{title}</h3>
+      <h3>{price}</h3>
+      {price > 30000 &&  <p>Discount of 5%</p>}
+     
+       
+    </div>
+  );
 }
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,15 +1,17 @@
 import Product from "./Product";
 
-function ProductTab(){
+function ProductTab() {
+    // eslint-disable-next-line no-unused-vars
+    let option = [ <li>"hitech"</li>,<li>"fast"</li>]
+
+
     return (
         <div>
-   
-        <Product title="phone"     price ={300}/>
-        <Product title="laptop"    price ={300}/>
-        <Product title="pen"    price ={300}/>
-      
-      </div>
-      )
+            <Product title="phone" price={30000} />
+            <Product title="laptop" price={40400} />
+            <Product title="pen" price={30} />
+        </div>
+    );
 }
 
 export default ProductTab;
